abb74d18da6be3922f4ce0e2e1e6b493b4b1061d
Dirkmal <kamsam403@gmail.com>
2019-09-18 16:47:26 +0100

    Added PHP controller

---
+++ controller.php
@@ -0,0 +1,27 @@
+<?php
+    Class Task1 {
+        public $message_types = array(1 => "success", 0 => "error");
+
+        /**
+         * @param string $title - The title of the alert
+         * @param string $message - The alert message
+         * @param int $type - The alert type, according to $message_types 
+         * Displays an alert box
+         */
+        function alert($title, $message, $type) {
+
+        }
+
+        function signIn($username, $password) {
+
+        }
+
+        function signOut($user_id) {
+
+        }
+
+        function signUp($email, $username, $password) {
+
+        }
+    }
+?>
